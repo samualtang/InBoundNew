1f7d00ac9e8796a191ab3fe27dcf98512c87c322
wolfbuyu <32753751+wolfbuyu@users.noreply.github.com>
2018-01-11 17:21:57 +0800

    打印布局

--- LabelPrint/LabelPrint/QueryForm.Designer.cs
+++ LabelPrint/LabelPrint/QueryForm.Designer.cs
@@ -33,7 +33,7 @@ namespace LabelPrint
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.btnPrint = new System.Windows.Forms.Button();
             this.tbNum = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
@@ -44,17 +44,17 @@ namespace LabelPrint
             this.btnSearch = new System.Windows.Forms.Button();
             this.tbName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.pager1 = new WHC.Pager.WinControl.Pager();
             this.ItemList = new System.Windows.Forms.DataGridView();
-            this.ItemCount = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Operate = new System.Windows.Forms.DataGridViewButtonColumn();
             this.iTEMNODataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.iTEMNAMEDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.sHORTNAMEDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.bIGBOXBARDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ItemCount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Operate = new System.Windows.Forms.DataGridViewButtonColumn();
             this.tWMSITEMBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.button2 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ItemList)).BeginInit();
@@ -74,28 +74,32 @@ namespace LabelPrint
             this.groupBox1.Controls.Add(this.btnSearch);
             this.groupBox1.Controls.Add(this.tbName);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(28, 12);
+            this.groupBox1.Location = new System.Drawing.Point(25, 10);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1084, 139);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(964, 116);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "请输入查询条件";
             // 
-            // button1
+            // button2
             // 
-            this.button1.Location = new System.Drawing.Point(964, 39);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(121, 38);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "入库单数据";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button2.Location = new System.Drawing.Point(769, 67);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 32);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "商品数据";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // btnPrint
             // 
-            this.btnPrint.Location = new System.Drawing.Point(844, 87);
+            this.btnPrint.Location = new System.Drawing.Point(645, 68);
+            this.btnPrint.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnPrint.Name = "btnPrint";
-            this.btnPrint.Size = new System.Drawing.Size(75, 36);
+            this.btnPrint.Size = new System.Drawing.Size(103, 30);
             this.btnPrint.TabIndex = 9;
             this.btnPrint.Text = "打印";
             this.btnPrint.UseVisualStyleBackColor = true;
@@ -103,58 +107,62 @@ namespace LabelPrint
             // 
             // tbNum
             // 
-            this.tbNum.Location = new System.Drawing.Point(606, 79);
+            this.tbNum.Location = new System.Drawing.Point(449, 66);
+            this.tbNum.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tbNum.Name = "tbNum";
-            this.tbNum.Size = new System.Drawing.Size(165, 28);
+            this.tbNum.Size = new System.Drawing.Size(147, 25);
             this.tbNum.TabIndex = 8;
             this.tbNum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tBox_KeyPress);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(505, 87);
+            this.label4.Location = new System.Drawing.Point(379, 74);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(53, 18);
+            this.label4.Size = new System.Drawing.Size(45, 15);
             this.label4.TabIndex = 7;
             this.label4.Text = "数量:";
             // 
             // tbjyCode
             // 
-            this.tbjyCode.Location = new System.Drawing.Point(136, 87);
+            this.tbjyCode.Location = new System.Drawing.Point(121, 72);
+            this.tbjyCode.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tbjyCode.Name = "tbjyCode";
-            this.tbjyCode.Size = new System.Drawing.Size(296, 28);
+            this.tbjyCode.Size = new System.Drawing.Size(152, 25);
             this.tbjyCode.TabIndex = 6;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(41, 90);
+            this.label3.Location = new System.Drawing.Point(36, 75);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(89, 18);
+            this.label3.Size = new System.Drawing.Size(75, 15);
             this.label3.TabIndex = 5;
             this.label3.Text = "件烟编码:";
             // 
             // tbCode
             // 
-            this.tbCode.Location = new System.Drawing.Point(606, 35);
+            this.tbCode.Location = new System.Drawing.Point(449, 29);
+            this.tbCode.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tbCode.Name = "tbCode";
-            this.tbCode.Size = new System.Drawing.Size(165, 28);
+            this.tbCode.Size = new System.Drawing.Size(147, 25);
             this.tbCode.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(469, 35);
+            this.label2.Location = new System.Drawing.Point(347, 31);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(89, 18);
+            this.label2.Size = new System.Drawing.Size(75, 15);
             this.label2.TabIndex = 3;
             this.label2.Text = "卷烟编码:";
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(844, 29);
+            this.btnSearch.Location = new System.Drawing.Point(645, 24);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(75, 36);
+            this.btnSearch.Size = new System.Drawing.Size(103, 30);
             this.btnSearch.TabIndex = 2;
             this.btnSearch.Text = "搜索";
             this.btnSearch.UseVisualStyleBackColor = true;
@@ -162,27 +170,41 @@ namespace LabelPrint
             // 
             // tbName
             // 
-            this.tbName.Location = new System.Drawing.Point(136, 35);
+            this.tbName.Location = new System.Drawing.Point(121, 29);
+            this.tbName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tbName.Name = "tbName";
-            this.tbName.Size = new System.Drawing.Size(296, 28);
+            this.tbName.Size = new System.Drawing.Size(152, 25);
             this.tbName.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(41, 38);
+            this.label1.Location = new System.Drawing.Point(36, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 18);
+            this.label1.Size = new System.Drawing.Size(75, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "卷烟名称:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(794, 32);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 32);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "入库单数据";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.pager1);
             this.groupBox2.Controls.Add(this.ItemList);
-            this.groupBox2.Location = new System.Drawing.Point(28, 157);
+            this.groupBox2.Location = new System.Drawing.Point(25, 131);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(1084, 800);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Size = new System.Drawing.Size(964, 667);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "结果集";
@@ -192,10 +214,11 @@ namespace LabelPrint
             this.pager1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pager1.CurrentPageIndex = 1;
             this.pager1.Cursor = System.Windows.Forms.Cursors.WaitCursor;
-            this.pager1.Location = new System.Drawing.Point(6, 585);
+            this.pager1.Location = new System.Drawing.Point(5, 487);
+            this.pager1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pager1.Name = "pager1";
             this.pager1.RecordCount = 0;
-            this.pager1.Size = new System.Drawing.Size(970, 65);
+            this.pager1.Size = new System.Drawing.Size(862, 54);
             this.pager1.TabIndex = 1;
             this.pager1.UseWaitCursor = true;
             this.pager1.Load += new System.EventHandler(this.pager1_Load);
@@ -212,30 +235,13 @@ namespace LabelPrint
             this.ItemCount,
             this.Operate});
             this.ItemList.DataSource = this.tWMSITEMBindingSource;
-            this.ItemList.Location = new System.Drawing.Point(6, 42);
+            this.ItemList.Location = new System.Drawing.Point(5, 35);
+            this.ItemList.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ItemList.Name = "ItemList";
             this.ItemList.RowTemplate.Height = 30;
-            this.ItemList.Size = new System.Drawing.Size(970, 537);
+            this.ItemList.Size = new System.Drawing.Size(862, 447);
             this.ItemList.TabIndex = 0;
             this.ItemList.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ItemList_CellContentClick);
-            // 
-            // ItemCount
-            // 
-            dataGridViewCellStyle1.NullValue = "1";
-            this.ItemCount.DefaultCellStyle = dataGridViewCellStyle1;
-            this.ItemCount.HeaderText = "打印份数";
-            this.ItemCount.Name = "ItemCount";
-            this.ItemCount.ToolTipText = "请输入打印份数";
-            // 
-            // Operate
-            // 
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle2.NullValue = "打印";
-            this.Operate.DefaultCellStyle = dataGridViewCellStyle2;
-            this.Operate.HeaderText = "打印";
-            this.Operate.Name = "Operate";
-            this.Operate.Text = "打印";
-            this.Operate.UseColumnTextForButtonValue = true;
             // 
             // iTEMNODataGridViewTextBoxColumn
             // 
@@ -265,28 +271,37 @@ namespace LabelPrint
             this.bIGBOXBARDataGridViewTextBoxColumn.Name = "bIGBOXBARDataGridViewTextBoxColumn";
             this.bIGBOXBARDataGridViewTextBoxColumn.ReadOnly = true;
             // 
+            // ItemCount
+            // 
+            dataGridViewCellStyle1.NullValue = "1";
+            this.ItemCount.DefaultCellStyle = dataGridViewCellStyle1;
+            this.ItemCount.HeaderText = "打印份数";
+            this.ItemCount.Name = "ItemCount";
+            this.ItemCount.ToolTipText = "请输入打印份数";
+            // 
+            // Operate
+            // 
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.NullValue = "打印";
+            this.Operate.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Operate.HeaderText = "打印";
+            this.Operate.Name = "Operate";
+            this.Operate.Text = "打印";
+            this.Operate.UseColumnTextForButtonValue = true;
+            // 
             // tWMSITEMBindingSource
             // 
             this.tWMSITEMBindingSource.DataSource = typeof(InBound.T_WMS_ITEM);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(936, 80);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(121, 38);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "商品数据";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // QueryForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1343, 944);
+            this.ClientSize = new System.Drawing.Size(1194, 787);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "QueryForm";
             this.Text = "件烟条码打印";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
