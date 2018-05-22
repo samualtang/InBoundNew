f189f9c082487033307c3758620c676934a7ec4d
yq18508404250 <39258266+yq18508404250@users.noreply.github.com>
2018-05-22 17:31:05 +0800

    UpUnnormalImage

--- UnNormal/UnNormalFm.Designer.cs
+++ UnNormal/UnNormalFm.Designer.cs
@@ -28,33 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UnNormalFm));
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UnNormalFm));
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button12 = new System.Windows.Forms.Button();
-            this.button11 = new System.Windows.Forms.Button();
-            this.button10 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.task_data = new System.Windows.Forms.DataGridView();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.list_data = new System.Windows.Forms.ListBox();
-            this.groupboxErr = new System.Windows.Forms.GroupBox();
-            this.listError = new System.Windows.Forms.ListBox();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -65,6 +53,18 @@
             this.finishqty = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.分拣线 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.percent = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.list_data = new System.Windows.Forms.ListBox();
+            this.groupboxErr = new System.Windows.Forms.GroupBox();
+            this.listError = new System.Windows.Forms.ListBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.task_data)).BeginInit();
@@ -101,54 +101,6 @@
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(1038, 43);
             this.panel3.TabIndex = 36;
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(406, -1);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(101, 43);
-            this.button7.TabIndex = 10;
-            this.button7.Text = "同步品牌数据";
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
-            // 
-            // button6
-            // 
-            this.button6.BackgroundImage = global::UnNormal.Properties.Resources.edit;
-            this.button6.Location = new System.Drawing.Point(299, -1);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(101, 43);
-            this.button6.TabIndex = 9;
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click_1);
-            // 
-            // button12
-            // 
-            this.button12.BackgroundImage = global::UnNormal.Properties.Resources.stop;
-            this.button12.Location = new System.Drawing.Point(97, 0);
-            this.button12.Name = "button12";
-            this.button12.Size = new System.Drawing.Size(99, 43);
-            this.button12.TabIndex = 8;
-            this.button12.UseVisualStyleBackColor = true;
-            // 
-            // button11
-            // 
-            this.button11.BackgroundImage = global::UnNormal.Properties.Resources.rfresh;
-            this.button11.Location = new System.Drawing.Point(194, 0);
-            this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(99, 43);
-            this.button11.TabIndex = 7;
-            this.button11.UseVisualStyleBackColor = true;
-            // 
-            // button10
-            // 
-            this.button10.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button10.BackgroundImage")));
-            this.button10.Location = new System.Drawing.Point(0, 0);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(99, 43);
-            this.button10.TabIndex = 0;
-            this.button10.UseVisualStyleBackColor = true;
-            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // button5
             // 
@@ -202,15 +154,6 @@
             this.button2.Text = "关闭";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // button1
-            // 
-            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(99, 43);
-            this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // task_data
             // 
             this.task_data.AllowUserToAddRows = false;
@@ -262,65 +205,6 @@
             this.task_data.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.task_data.Size = new System.Drawing.Size(579, 239);
             this.task_data.TabIndex = 30;
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.label5);
-            this.panel2.Controls.Add(this.label2);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 400);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1038, 19);
-            this.panel2.TabIndex = 37;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(825, 3);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(0, 12);
-            this.label5.TabIndex = 2;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 4);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 12);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "提示信息";
-            // 
-            // list_data
-            // 
-            this.list_data.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.list_data.FormattingEnabled = true;
-            this.list_data.ItemHeight = 12;
-            this.list_data.Location = new System.Drawing.Point(0, 288);
-            this.list_data.Name = "list_data";
-            this.list_data.Size = new System.Drawing.Size(1038, 112);
-            this.list_data.TabIndex = 39;
-            // 
-            // groupboxErr
-            // 
-            this.groupboxErr.Controls.Add(this.listError);
-            this.groupboxErr.Dock = System.Windows.Forms.DockStyle.Right;
-            this.groupboxErr.Font = new System.Drawing.Font("宋体", 11F);
-            this.groupboxErr.Location = new System.Drawing.Point(598, 43);
-            this.groupboxErr.Name = "groupboxErr";
-            this.groupboxErr.Size = new System.Drawing.Size(440, 245);
-            this.groupboxErr.TabIndex = 40;
-            this.groupboxErr.TabStop = false;
-            this.groupboxErr.Text = "故障信息";
-            // 
-            // listError
-            // 
-            this.listError.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listError.FormattingEnabled = true;
-            this.listError.ItemHeight = 15;
-            this.listError.Location = new System.Drawing.Point(3, 20);
-            this.listError.Name = "listError";
-            this.listError.Size = new System.Drawing.Size(434, 222);
-            this.listError.TabIndex = 0;
             // 
             // Column1
             // 
@@ -382,6 +266,123 @@
             this.percent.HeaderText = "完成百分比";
             this.percent.Name = "percent";
             this.percent.ReadOnly = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 400);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1038, 19);
+            this.panel2.TabIndex = 37;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(825, 3);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 12);
+            this.label5.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "提示信息";
+            // 
+            // list_data
+            // 
+            this.list_data.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.list_data.FormattingEnabled = true;
+            this.list_data.ItemHeight = 12;
+            this.list_data.Location = new System.Drawing.Point(0, 288);
+            this.list_data.Name = "list_data";
+            this.list_data.Size = new System.Drawing.Size(1038, 112);
+            this.list_data.TabIndex = 39;
+            // 
+            // groupboxErr
+            // 
+            this.groupboxErr.Controls.Add(this.listError);
+            this.groupboxErr.Dock = System.Windows.Forms.DockStyle.Right;
+            this.groupboxErr.Font = new System.Drawing.Font("宋体", 11F);
+            this.groupboxErr.Location = new System.Drawing.Point(598, 43);
+            this.groupboxErr.Name = "groupboxErr";
+            this.groupboxErr.Size = new System.Drawing.Size(440, 245);
+            this.groupboxErr.TabIndex = 40;
+            this.groupboxErr.TabStop = false;
+            this.groupboxErr.Text = "故障信息";
+            // 
+            // listError
+            // 
+            this.listError.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listError.FormattingEnabled = true;
+            this.listError.ItemHeight = 15;
+            this.listError.Location = new System.Drawing.Point(3, 20);
+            this.listError.Name = "listError";
+            this.listError.Size = new System.Drawing.Size(434, 222);
+            this.listError.TabIndex = 0;
+            // 
+            // button7
+            // 
+            this.button7.BackgroundImage = global::UnNormal.Properties.Resources.GetDataOnline;
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button7.Location = new System.Drawing.Point(406, 0);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(108, 43);
+            this.button7.TabIndex = 10;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = global::UnNormal.Properties.Resources.edit;
+            this.button6.Location = new System.Drawing.Point(299, 0);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(101, 43);
+            this.button6.TabIndex = 9;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click_1);
+            // 
+            // button12
+            // 
+            this.button12.BackgroundImage = global::UnNormal.Properties.Resources.stop;
+            this.button12.Location = new System.Drawing.Point(97, 0);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(99, 43);
+            this.button12.TabIndex = 8;
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.BackgroundImage = global::UnNormal.Properties.Resources.rfresh;
+            this.button11.Location = new System.Drawing.Point(194, 0);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(99, 43);
+            this.button11.TabIndex = 7;
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button10.BackgroundImage")));
+            this.button10.Location = new System.Drawing.Point(0, 0);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(99, 43);
+            this.button10.TabIndex = 0;
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 43);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // UnNormalFm
             // 
