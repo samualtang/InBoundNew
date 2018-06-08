17c7b65d14493ff8f02fd8c1af1307f7a351db19
samualtang <83004808@qq.com>
2018-06-08 16:48:08 +0800

    update 20180608 001

---
+++ InBound/Model/UnionTaskInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InBound.Model
+{
+    public class UnionTaskInfo
+    {
+        public String CIGARETTDENAME { get; set; }
+        public String CIGARETTDECODE { get; set; }
+        public decimal MainBelt { get; set; }
+        public decimal SortNum { get; set; }
+        public decimal qty { get; set; }
+    }
+}
