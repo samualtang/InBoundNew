af9c01fc1842469a6271d349809cda941b9b0b11
samual <83004808@qq.com>
2019-03-05 09:16:27 +0800

    itemgroup

---
+++ InBound/Model/ItemGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InBound.Model
+{
+   public class ItemGroup
+    {
+        public String CigaretteCode { get; set; }
+        public decimal Total { get; set; }
+    }
+}
