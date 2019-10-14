4bc562ae054c0e2f75f604d80dd3c48b4fa8b8a3
yq18508404250 <39258266+yq18508404250@users.noreply.github.com>
2019-10-14 15:51:48 +0800

    Update w_SortingControlMain.cs

--- SortingControlSys/SortingControlSys/SortingControl/w_SortingControlMain.cs
+++ SortingControlSys/SortingControlSys/SortingControl/w_SortingControlMain.cs
@@ -444,15 +444,15 @@ namespace SortingControlSys.SortingControl
                             {
                                 taskno = decimal.Parse(UnionGroup.ReadD((int)((i - 1) * 16 + 2 * (sortgroup - 1))).ToString());
 
-                                zqqty = decimal.Parse(UnionGroup.ReadD((int)((i - 1) * 16 + 2 * (sortgroup - 1) - 1)).ToString());
+                                zqqty = decimal.Parse(UnionGroup.ReadD((int)((i - 1) * 16 + 2 * (sortgroup - 1) + 1)).ToString());
                             }
                             catch
                             {
                                 taskno = 0;
                                 zqqty = 0;
                             }
-                            writeLog.Write("plc地址:" + plclist[(int)((i - 1) * 16 + 2 * (sortgroup - 1))] + "读取组:" + sortgroupno2 + " 主皮带:" + i + " 合流任务号:" + taskno + " 抓取数量:" + zqqty);
-                            updateListBox("plc地址:" + plclist[(int)((i - 1) * 16 + 2 * (sortgroup - 1))] + "读取组:" + sortgroupno2 + " 主皮带:" + i + " 合流任务号:" + taskno + " 抓取数量:" + zqqty);
+                            writeLog.Write("plc地址:" + plclist[(int)((i - 1) * 16 + 2 * (sortgroup - 1))] + "读取组:" + sortgroupno2 + " 主皮带:" + i + " 合流任务号:" + taskno + "；plc地址：" + plclist[(int)((i - 1) * 16 + 2 * (sortgroup - 1) + 1)] + " 抓取数量:" + zqqty);
+                            updateListBox("plc地址:" + plclist[(int)((i - 1) * 16 + 2 * (sortgroup - 1))] + "读取组:" + sortgroupno2 + " 主皮带:" + i + " 合流任务号:" + taskno + "；plc地址：" + plclist[(int)((i - 1) * 16 + 2 * (sortgroup - 1) + 1 )] + " 抓取数量:" + zqqty);
                             if (!ProducePokeService.CheckExistTaskNo(taskno))
                             {
                                 zqqty = 0;
@@ -648,8 +648,8 @@ namespace SortingControlSys.SortingControl
                                 taskno = 0;
                                 zqqty = 0;
                             }
-                            writeLog.Write("plc地址:" + plclist[(int)((i - 1) * 16 + 2 * (sortgroup - 1))] + "读取组:" + sortgroupno1 + " 主皮带:" + i + " 合流任务号:" + taskno + " 抓取数量:" + zqqty);
-                            updateListBox("plc地址:" + plclist[(int)((i - 1) * 16 + 2 * (sortgroup - 1))] + "读取组:" + sortgroupno1 + " 主皮带:" + i + " 合流任务号:" + taskno + " 抓取数量:" + zqqty);
+                            writeLog.Write("plc地址:" + plclist[(int)((i - 1) * 16 + 2 * (sortgroup - 1))] + "读取组:" + sortgroupno1 + " 主皮带:" + i + " 合流任务号:" + taskno + "；抓取数量地址：" + plclist[(int)((i - 1) * 16 + 2 * (sortgroup) - 1)] + " 抓取数量:" + zqqty);
+                            updateListBox("plc地址:" + plclist[(int)((i - 1) * 16 + 2 * (sortgroup - 1))] + "读取组:" + sortgroupno1 + " 主皮带:" + i + " 合流任务号:" + taskno + "；抓取数量地址：" + plclist[(int)((i - 1) * 16 + 2 * (sortgroup) - 1)] + " 抓取数量:" + zqqty);
                             if (!ProducePokeService.CheckExistTaskNo(taskno))
                             {
                                 zqqty = 0;
